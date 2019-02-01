a8de6cdc9da3f52603a2ca6ad6324c79318adb86
Truong Hoang <truong.hoang@bynk.se>
2019-02-01 14:57:11 +0100

    Add CORS

--- slio.py
+++ slio.py
@@ -2,17 +2,20 @@ import requests
 from functional import seq
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
 import yaml
 
 app = Flask(__name__)
+CORS(app)
+
 cfg = yaml.safe_load(open('config.yaml', 'r'))
 key = cfg["key"]
 
 
 @app.route("/")
 def get_time():
-    home = requests.get('http://api.sl.se/api2/realtimedeparturesV4.json?key={}&siteid=1555&timewindow=20&bus=false&train=false&metro=false&ship=false'.format(key)).json()
-    work = requests.get('http://api.sl.se/api2/realtimedeparturesV4.json?key={}&siteid=9325&timewindow=20&bus=false&train=false&metro=false&ship=false'.format(key)).json()
+    home = requests.get('http://api.sl.se/api2/realtimedeparturesV4.json?key={}&siteid=1555&timewindow=200&bus=false&train=false&metro=false&ship=false'.format(key)).json()
+    work = requests.get('http://api.sl.se/api2/realtimedeparturesV4.json?key={}&siteid=9325&timewindow=200&bus=false&train=false&metro=false&ship=false'.format(key)).json()
 
     tramshome = home['ResponseData']['Trams']
     tramswork = work['ResponseData']['Trams']
